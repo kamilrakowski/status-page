6eed77ea4deff187bcb6b5c4a40ead30f8fb5115
Flashwave <julianvdg@gmail.com>
2014-10-03 17:18:45 +0200

    Update index.php
    Quick way of solving script paths not found, I'll probably change this later.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ function getHeaders($url) {
 	curl_setopt($curl,	CURLOPT_URL,			$url);
 	curl_setopt($curl,	CURLOPT_HEADER,			true);
 	curl_setopt($curl,	CURLOPT_NOBODY,			true);
-	curl_setopt($curl,	CURLOPT_RETURNTRANSFER,		true);
-	curl_setopt($curl,	CURLOPT_CONNECTTIMEOUT,		2);
+	curl_setopt($curl,	CURLOPT_RETURNTRANSFER,	true);
+	curl_setopt($curl,	CURLOPT_CONNECTTIMEOUT,	2);
 	curl_setopt($curl,	CURLOPT_TIMEOUT,		4);
 	curl_setopt($curl,	CURLOPT_USERAGENT,		'Mozilla/5.0 (compatible; FlashiiStatus/2.0)');
 	
@@ -19,6 +19,12 @@ function getHeaders($url) {
 
 if(isset($_GET['url'])) {
 	print getHeaders($_GET['url']);
+	exit;
+}
+
+if($_SERVER['REQUEST_URI'] != $_SERVER['PHP_SELF']) {
+	header('Location: /index.php');
+	print '<meta http-equiv="refresh" content="0; url=/index.php" />';
 	exit;
 }
 ?> 
